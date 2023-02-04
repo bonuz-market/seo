51e7a109f29c1ef9d1b4dd59fbf060c765dd7a07
Mostafa Talaat <mostafatalaat770@gmail.com>
2023-02-04 15:28:54 +0200

    add types
    Signed-off-by: Mostafa Talaat <mostafatalaat770@gmail.com>

---
+++ src/lib/types.ts
@@ -0,0 +1,10 @@
+export interface Policy {
+	allow: string | string[];
+	disallow: string | string[];
+	userAgent?: string;
+}
+
+export interface Robots {
+	policies: Policy | Policy[];
+	sitemap?: string | string[];
+}
