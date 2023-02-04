026ce9492d54a3c5476043c28cd8a2a53f0bfa66
Mostafa Talaat <mostafatalaat770@gmail.com>
2023-02-04 15:50:59 +0200

    refactor and extract functions to helpers file
    Signed-off-by: Mostafa Talaat <mostafatalaat770@gmail.com>

--- src/lib/robots.ts
+++ src/lib/robots.ts
@@ -1,53 +1,12 @@
-import { Policy, Robots } from "./types";
-
-const createNewLine = (addNewLine: boolean) => {
-	return addNewLine ? "\n" : "";
-};
-
-const hasBaseUserAgent = (policy: Policy | Policy[]) => {
-	if (Array.isArray(policy)) {
-		return policy[0].userAgent;
-	}
-	return policy.userAgent;
-};
-
-const createPolicy = (policy: Policy, newLine: boolean = false) => {
-	const { allow, disallow, userAgent } = policy;
-
-	const userAgentString = userAgent ? `User-agent: ${userAgent}` : "";
-
-	const userAgentStringFormatted = userAgentString
-		? `${createNewLine(newLine)}${userAgentString}\n`
-		: "";
-
-	const allowString = Array.isArray(allow)
-		? allow.map((a) => `Allow: ${a}`).join("\n")
-		: `Allow: ${allow}`;
-
-	const disallowString = Array.isArray(disallow)
-		? disallow.map((d) => `Disallow: ${d}`).join("\n")
-		: `Disallow: ${disallow}`;
-
-	return `${userAgentStringFormatted}${allowString}\n${disallowString}`;
-};
+import { createPolcies, createRules, hasBaseUserAgent } from "./helpers";
+import { Robots } from "./types";
 
 export const createRobots = ({ policies, sitemap }: Robots) => {
-	const policiesString = Array.isArray(policies)
-		? policies.map((p, i) => createPolicy(p, i !== 0)).join("\n")
-		: createPolicy(policies);
+	const userAgentString = hasBaseUserAgent(policies) ? "" : "User-agent: *\n";
 
-	let sitemapString = "";
-	if (sitemap) {
-		sitemapString = Array.isArray(sitemap)
-			? sitemap.map((s) => `Sitemap: ${s}`).join("\n")
-			: `Sitemap: ${sitemap}`;
-	}
+	const policiesString = createPolcies(policies);
 
-	let userAgentString = "";
-
-	if (!hasBaseUserAgent(policies)) {
-		userAgentString = "User-agent: *\n";
-	}
+	const sitemapString = createRules("Sitemap", sitemap);
 
 	return `${userAgentString}${policiesString}\n${sitemapString}`;
 };
