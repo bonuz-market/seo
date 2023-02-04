54a6d34ed40bbf2afb18db5f548c024fdef89bc3
Mostafa Talaat <mostafatalaat770@gmail.com>
2023-02-04 19:48:27 +0200

    add rollup
    Signed-off-by: Mostafa Talaat <mostafatalaat770@gmail.com>

---
+++ rollup.config.mjs
@@ -0,0 +1,32 @@
+import dts from "rollup-plugin-dts";
+import esbuild from "rollup-plugin-esbuild";
+
+export default [
+	{
+		input: `src/index.ts`,
+		plugins: [
+			esbuild({
+				minify: true,
+			}),
+		],
+		output: [
+			{
+				file: `dist/index.js`,
+				format: "cjs",
+			},
+			{
+				file: `dist/index.mjs`,
+				format: "es",
+			},
+		],
+	},
+
+	{
+		input: `src/index.ts`,
+		plugins: [dts()],
+		output: {
+			file: `dist/index.d.ts`,
+			format: "es",
+		},
+	},
+];
