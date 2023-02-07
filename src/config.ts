f1e829725f17dd53806e8e5ff273e1d7752c9c54
Oliver Molnar <403access@olimo.me>
2023-02-07 14:30:46 +0400

    feat: add robot config retrieval by env

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,11 @@
 import { Robots } from './types'
 
+export const getRobotsConfig = (env: string, allow: string[] = [], disallow: string[] = []): Robots => {
+  if (allow.findIndex((entry) => entry === env)) return allowAll()
+  else if (disallow.length === 0 || disallow.findIndex((entry) => entry === env)) return disallowAll()
+  else throw new Error(`Unexpected env '${env}'.`)
+}
+
 const allowOrDisallowAll = (propertyName: 'allow' | 'disallow'): Robots => {
   return {
     policies: [
